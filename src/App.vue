01504c66c4bd8e834ed58de9dad6d04b1950784b
藤原未来 <www.motimoti12@gmail.com>
2021-05-19 22:42:57 +0900

    add: テストで追加

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <LikeHeader></LikeHeader>
+    <LikeNumber></LikeNumber>
+    <a href="">テスト</a>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LikeHeader from "./components/LikeHeader.vue";
+import LikeNumber from "./components/LikeNumber.vue";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    LikeHeader,
+    LikeNumber
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss" scoped>
+div {
+  border: 1px solid blue;
+  a{
+    width: 100px;
+    background-color: aqua;
+  }
 }
 </style>
+
