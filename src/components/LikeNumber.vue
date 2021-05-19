4d87e22dfee2b5934e901f010b1751265c4ec74b
藤原未来 <www.motimoti12@gmail.com>
2021-05-19 22:46:42 +0900

    first commit

---
+++ src/components/LikeNumber.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <p>いいね({{ number }})</p>
+    <button @click="increment">+1</button>
+    <a href="">aaaaaaa</a>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      number: 5
+    };
+  },
+  methods: {
+    increment() {
+      this.number += 1;
+    }
+  }
+};
+</script>
+
+<style scoped>
+div {
+  border: 1px solid red;
+}
+</style>
+
